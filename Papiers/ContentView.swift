4caa97a7d6f7078417a25121778c749bee7699b2
Yichen Xu <yichen.x@outlook.com>
2021-07-16 22:13:42 +0800

    Initial Commit

---
+++ Papiers/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Papiers
+//
+//  Created by Yichen Xu on 2021/7/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Papiers/PapiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  PapiersApp.swift
+//  Papiers
+//
+//  Created by Yichen Xu on 2021/7/16.
+//
+
+import SwiftUI
+
+@main
+struct PapiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
